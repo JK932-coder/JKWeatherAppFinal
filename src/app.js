402e409b9225e9837569bc044236d54b59ce8227
JK932-coder <ekaterina.kosmaciova@gmail.com>
2024-09-18 13:57:59 +0100

    Injected weather data including wind forecast, time and day and humidity. Formatted also time and day properly

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@ function refreshWeather (response) {
     let temperatureElement =document.querySelector("#temperature");
     let temperature=response.data.temperature.current; 
     let cityElement=document.querySelector("#city"); 
+    let descriptionElement = document.querySelector("#description"); 
+    let humidityElement = document.querySelector("#humidity"); 
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date (response.data.time *1000); 
+    console.log(response.data); 
+ 
     cityElement.innerHTML=response.data.city; 
+    descriptionElement.innerHTML=response.data.condition.description; 
     temperatureElement.innerHTML= Math.round(temperature); 
+    humidityElement.innerHTML=`${response.data.temperature.humidity}%`; 
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`; 
+    timeElement.innerHTML = formatDate(date);
 }
+function formatDate(date){
+   let days = [
+     "Sunday",
+     "Monday",
+     "Tuesday",
+     "Wednesday",
+     "Thursday",
+     "Friday",
+     "Saturday",
+     "Sunday",
+   ]; 
+   let day = days[date.getDay()]; 
+   let minutes =date.getMinutes(); 
+   let hours=date.getHours(); 
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
+  if (hours < 10) {
+    hours = `0${hours}`;
+    
+}
+    return `${day} ${hours}:${minutes}`; }
+    
 function searchCity (city) {
 let apiKey = "43008a6ae95edac1643c1odtb4f58d13";
 let apiUrl =
