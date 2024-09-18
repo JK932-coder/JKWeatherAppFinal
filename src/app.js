2fae1ed41a980f4e4e7fea563c95b3dbd2fc5a8d
JK932-coder <ekaterina.kosmaciova@gmail.com>
2024-09-18 14:31:16 +0100

    Fixed an issue with minutes not displaying correctly

--- src/app.js
+++ src/app.js
@@ -1,58 +1,57 @@
-function refreshWeather (response) {
-    let temperatureElement =document.querySelector("#temperature");
-    let temperature=response.data.temperature.current; 
-    let cityElement=document.querySelector("#city"); 
-    let descriptionElement = document.querySelector("#description"); 
-    let humidityElement = document.querySelector("#humidity"); 
-    let windSpeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time");
-    let date = new Date (response.data.time *1000); 
-    console.log(response.data); 
- 
-    cityElement.innerHTML=response.data.city; 
-    descriptionElement.innerHTML=response.data.condition.description; 
-    temperatureElement.innerHTML= Math.round(temperature); 
-    humidityElement.innerHTML=`${response.data.temperature.humidity}%`; 
-    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`; 
-    timeElement.innerHTML = formatDate(date);
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let timestamp = response.data.time;
+  let date = new Date(timestamp * 1000);
+
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
 }
-function formatDate(date){
-   let days = [
-     "Sunday",
-     "Monday",
-     "Tuesday",
-     "Wednesday",
-     "Thursday",
-     "Friday",
-     "Saturday",
-     "Sunday",
-   ]; 
-   let day = days[date.getDay()]; 
-   let minutes =date.getMinutes(); 
-   let hours=date.getHours(); 
-    if (minutes < 10) {
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
   if (hours < 10) {
     hours = `0${hours}`;
-    
-}
-    return `${day} ${hours}:${minutes}`; }
-    
-function searchCity (city) {
-let apiKey = "43008a6ae95edac1643c1odtb4f58d13";
-let apiUrl =
-  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(refreshWeather); 
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
+function searchCity(city) {
+  let apiKey = "43008a6ae95edac1643c1odtb4f58d13";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 
-function handleSearchSubmit (event) {
-    event.preventDefault(); 
-    let searchInput = document.querySelector("#search-form-input"); 
-    searchCity(searchInput.value); 
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit); 
-searchCity("London"); 
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("London");
